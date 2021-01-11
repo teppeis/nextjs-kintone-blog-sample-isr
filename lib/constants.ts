d12fd8a55dec427ccf65bf57299469c7be0fe071
teppeis <teppeis@gmail.com>
2021-01-11 13:36:26 +0900

    fix: repository URL and blog title

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,7 @@
-export const EXAMPLE_PATH = 'blog-starter-typescript'
-export const CMS_NAME = 'Markdown'
+export const REPO_URL =
+  "https://github.com/teppeis/nextjs-kintone-blog-sample-isr";
+export const CMS_NAME = "Kintone";
+export const CMS_URL = "https://www.kintone.com/";
+export const BLOG_TITLE = "Next.js + Kintone Blog.";
 export const HOME_OG_IMAGE_URL =
-  'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
+  "https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
