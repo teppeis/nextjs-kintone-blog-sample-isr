141ee2404a09fa4dce624f14349a26cfaf46daca
teppeis <teppeis@gmail.com>
2021-01-11 14:04:23 +0900

    fix: title

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,6 +2,6 @@ export const REPO_URL =
   "https://github.com/teppeis/nextjs-kintone-blog-sample-isr";
 export const CMS_NAME = "Kintone";
 export const CMS_URL = "https://www.kintone.com/";
-export const BLOG_TITLE = "Next.js + Kintone Blog.";
+export const BLOG_TITLE = "Next.js + Kintone Blog (ISR)";
 export const HOME_OG_IMAGE_URL =
   "https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
